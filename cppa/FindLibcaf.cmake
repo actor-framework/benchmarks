40311f9a0ad2f06d525d635a795811962b5e628a
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2014-07-19 02:23:41 +0200

    Migrate to libcaf

--- cppa/FindLibcaf.cmake
+++ cppa/FindLibcaf.cmake
@@ -1,46 +1,67 @@
-# - Try to find libcppa
+# - Try to find libcaf
 # Once done this will define
 #
-#  CPPA_FOUND    - system has libcppa
-#  CPPA_INCLUDE  - libcppa include dir
-#  CPPA_LIBRARY  - link againgst libcppa
-#  CPPA_VERSION  - version in {major}.{minor}.{patch} format
+#  LIBCAF_FOUND        - system has libcaf
+#  LIBCAF_INCLUDE_DIRS - libcaf include dir
+#  LIBCAF_LIBRARIES    - link againgst libcaf
+#  LIBCAF_VERSION      - version in {major}.{minor}.{patch} format
 
-find_path(LIBCAF_INCLUDE_DIR
-          NAMES
-            caf/all.hpp
-          HINTS
-            ${LIBCAF_ROOT_DIR}/include
-            /usr/include
-            /usr/local/include
-            /opt/local/include
-            /sw/include
-            ${CMAKE_INSTALL_PREFIX}/include
-            ../libcppa
-            ../../libcppa
-            ../../../libcppa)
+#find_package_handle_standard_args(libcaf DEFAULT_MSG
+#  LIBCAF_LIBRARIES
+#  LIBCAF_INCLUDE_DIRS)
 
-find_library(LIBCAF_LIBRARIES
-             NAMES
-               caf
-             HINTS
-               ${LIBCAF_ROOT_DIR}/lib
-               /usr/lib
-               /usr/local/lib
-               /opt/local/lib
-               /sw/lib
-               ${CMAKE_INSTALL_PREFIX}/lib
-               ../libcppa/build/lib
-               ../../libcppa/build/lib
-               ../../../libcppa/build/lib)
+foreach (comp ${Libcaf_FIND_COMPONENTS})
+  string(TOUPPER "${comp}" UPPERCOMP)
+  if ("${comp}" STREQUAL "core")
+    set(HDRNAME "caf/all.hpp")
+  else ()
+    set(HDRNAME "caf/${comp}/all.hpp")
+  endif ()
+  message(STATUS "Search for libcaf_${comp} and ${HDRNAME}")
+  set(HDRHINT "actor-framework/libcaf_${comp}")
+  unset(LIBCAF_INCLUDE_DIR)
+  find_path(LIBCAF_INCLUDE_DIR_${UPPERCOMP}
+            NAMES
+              ${HDRNAME}
+            HINTS
+              ${LIBCAF_ROOT_DIR}/include
+              /usr/include
+              /usr/local/include
+              /opt/local/include
+              /sw/include
+              ${CMAKE_INSTALL_PREFIX}/include
+              ../${HDRHINT}
+              ../../${HDRHINT}
+              ../../../${HDRHINT})
+  mark_as_advanced(LIBCAF_INCLUDE_DIR_${UPPERCOMP})
+  if ("${LIBCAF_INCLUDE_DIR_${UPPERCOMP}}" STREQUAL "LIBCAF_INCLUDE_DIR_${UPPERCOMP}-NOTFOUND")
+    break ()
+  else ()
+    message(STATUS "Found headers for ${HDRNAME}")
+  endif ()
+  find_library(LIBCAF_LIBRARY_${UPPERCOMP}
+               NAMES
+                 "caf_${comp}"
+               HINTS
+                 ${LIBCAF_ROOT_DIR}/lib
+                 /usr/lib
+                 /usr/local/lib
+                 /opt/local/lib
+                 /sw/lib
+                 ${CMAKE_INSTALL_PREFIX}/lib
+                 ../actor-framework/build/lib
+                 ../../actor-framework/build/lib
+                 ../../../actor-framework/build/lib)
+  mark_as_advanced(LIBCAF_LIBRARY_${UPPERCOMP})
+  if ("${LIBCAF_LIBRARY_${UPPERCOMP}}" STREQUAL "LIBCAF_LIBRARY-NOTFOUND")
+    break ()
+  else ()
+    message(STATUS "Found lib: ${LIBCAF_LIBRARY_${UPPERCOMP}}")
+  endif ()
+endforeach ()
 
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(libcaf DEFAULT_MSG
-  LIBCAF_LIBRARIES
-  LIBCAF_INCLUDE_DIR)
-
 mark_as_advanced(
   LIBCAF_ROOT_DIR
-  LIBCAF_LIBRARIES
-  LIBCAF_INCLUDE_DIR)
+  LIBCAF_INCLUDE_DIRS)
