ec31c7da8e03843039bd5a01b4aa7f72e0f3131b
Marian Triebe <m.triebe@live.de>
2014-07-30 20:05:55 +0200

    Add actor foundry benchmarks (2/3) This include the actor-creation benchmark and the mailbox benchmark

---
+++ actor_foundry/mailbox_performance/sender.java
@@ -0,0 +1,17 @@
+package osl.examples.caf_benches.mailbox_performance;
+
+import osl.manager.*;
+import osl.util.*;
+import osl.manager.annotations.message;
+
+public class sender extends Actor {
+  private static final long serialVersionUID = 4273990623755478312L;
+  public static final String _CLASS =
+                      "osl.examples.caf_benches.mailbox_performance.sender";
+  @message
+  public void run(ActorName whom, Long count) {
+    for (long i = 0; i < count; ++i) {
+      send(whom, "msg");
+    }
+  }
+}
