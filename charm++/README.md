ce5cb224579420fd8ae6a84871aea2b3e9c6c346
Alex Mantel <alexmantel93@gmail.com>
2014-07-31 12:15:05 +0000

    Add actor_creation for charm++

---
+++ charm++/README.md
@@ -0,0 +1,12 @@
+Charm++ Benchmark
+=================
+
+    ~ # git clone http://charm.cs.uiuc.edu/gerrit/charm
+    ~ # cd charm
+    ~ # ./build  
+
+Build
+-----
+To build benchmark-programs simply type `make` in current
+archieve.
+
---
+++ charm++/creation/creation.C
@@ -0,0 +1,65 @@
+#include <iostream>
+
+#include "charm++.h"
+#include "creation.decl.h"
+
+
+class spread_token : public CMessage_spread_token { 
+ public:
+  int value;
+};
+
+class result_token : public CMessage_result_token {
+ public:
+  int result;
+};
+
+struct main : public CBase_main {
+   void usage() {
+     using namespace std;
+     cout << endl << endl
+          << "invalid argument." << endl 
+          << "./creation (1-9)*" << endl
+          << " creates 2^POW actors" << endl << endl;
+   }   
+   main(CkArgMsg* m) {
+     using namespace std;
+     if (m->argc != 2) {
+       usage();
+       CkExit();
+     }   
+     int s_num = atoi(m->argv[1]);
+     CProxy_testee::ckNew(s_num, true, CProxy_testee());
+   }   
+};
+
+class testee : public CBase_testee {
+  testee_SDAG_CODE
+
+  CProxy_testee m_parent;
+  bool          m_is_root;
+public:
+  
+  testee(int s_num, bool is_root, CProxy_testee parent):
+  m_parent(parent), m_is_root(is_root) {
+    calc(s_num); 
+  } 
+
+  void respond(int val, int val2) {
+    if(m_is_root) {
+      if(val == val2) {
+        CkExit();
+      } else {
+        using namespace std;
+        cout << "Result is:" << val << " + "
+             << val2 << endl;
+        CkExit();
+      }
+    } else {
+      m_parent.response(1 + val + val2);
+      delete this;
+    }
+  }
+};
+
+#include "creation.def.h"
