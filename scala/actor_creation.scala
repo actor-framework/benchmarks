f7e8ad5e22773022cf39f6d90ad3833000324524
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2014-08-02 19:42:22 +0200

    Streamline scala actor creation

--- scala/actor_creation.scala
+++ scala/actor_creation.scala
@@ -4,59 +4,55 @@ import org.caf.utility._
 
 import akka.actor._
 
-case object GoAhead
-
 case class Spread(value: Int)
 case class Result(value: Int)
 
-class Testee(parent: ActorRef) extends Actor {
-    def receive = {
-        case Spread(1) =>
-            parent ! Result(1)
-            context.stop(self)
-        case Spread(s) =>
-            val msg = Spread(s-1)
-            context.actorOf(Props(new Testee(self))) ! msg
-            context.actorOf(Props(new Testee(self))) ! msg
-            context.become {
-                case Result(r1) =>
-                    context.become {
-                        case Result(r2) =>
-                            parent ! Result(1 + r1 + r2)
-                            context.stop(self)
-                    }
-            }
-    }
+object static_vars {
+  var cmdline_arg = 0
 }
 
-class RootTestee(n: Int) extends Actor {
-    def receive = {
-        case GoAhead =>
-            context.actorOf(Props(new Testee(self))) ! Spread(n)
-        case Result(v) =>
-            val res = v + 1
-            val expected = 1 << n
-            if (res != expected) {
-                Console.println("Expected " + expected + ", received " + res)
-                System.exit(42)
+class Testee(parent: ActorRef) extends Actor {
+  def receive = {
+    case Spread(1) =>
+      parent ! Result(1)
+      context.stop(self)
+    case Spread(s) =>
+      val msg = Spread(s - 1)
+      context.actorOf(Props(new Testee(self))) ! msg
+      context.actorOf(Props(new Testee(self))) ! msg
+      context.become {
+        case Result(r1) =>
+          context.become {
+            case Result(r2) =>
+              if (parent == null) {
+                val res = 2 + r1 + r2;
+                val expected = (1 << static_vars.cmdline_arg)
+                if (res != expected) {
+                  Console.println("Expected " + expected + ", found " + res)
+                  System.exit(42)
+                }
+              } else {
+                parent ! Result(1 + r1 + r2)
+              }
+              context.stop(self)
             }
-            global_latch.countDown
-            context.stop(self)
-    }
+          }
+  }
 }
 
 object actor_creation {
-    def usage() {
-        Console println "usage: POW\n       creates 2^POW actors"
+  def usage() {
+    Console println "usage: POW\n       creates 2^POW actors"
+  }
+  def main(args: Array[String]): Unit = args match {
+    case Array(IntStr(n)) => {
+      static_vars.cmdline_arg = n;
+      val system = ActorSystem()
+      system.actorOf(Props(new Testee(null))) ! Spread(n)
+      global_latch.await
+      system.shutdown
+      System.exit(0)
     }
-    def main(args: Array[String]): Unit = args match {
-        case Array(IntStr(n)) => {
-            val system = ActorSystem()
-            system.actorOf(Props(new RootTestee(n))) ! GoAhead
-            global_latch.await
-            system.shutdown
-            System.exit(0)
-        }
-        case _ => usage
-    }
+    case _ => usage
+  }
 }
