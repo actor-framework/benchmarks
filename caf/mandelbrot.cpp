958d62c75c93986a7e6abe8d7afedb4452a01c82
Joseph Noir <joseph.noir@gmail.com>
2014-10-05 11:43:21 +0200

    Add mandelbrot benchmark for scala, erlang and caf
    Uses benchmarks from the Computer Language Benchmarks Game
    (http://benchmarksgame.alioth.debian.org/u64q/performance.php?test=mandelbrot),
    with adjustments for actors.

---
+++ caf/mandelbrot.cpp
@@ -0,0 +1,83 @@
+#include <cstdio>
+#include <cstdlib>
+
+#include <vector>
+#include <iostream>
+
+#include "caf/all.hpp"
+
+typedef unsigned char byte;
+
+using namespace std;
+using namespace caf;
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    cout << "usage: ./" << argv[0] << " N" << endl;
+    return 0;
+  }
+  const int    N              = atoi(argv[1]);
+  const int    width          = N;
+  const int    height         = N;
+  const int    max_x          = (width + 7) / 8;
+  const int    max_iterations = 50;
+  const double limit          = 2.0;
+  const double limit_sq       = limit * limit;
+
+  vector<byte> buffer(height * max_x);
+  vector<double> cr0(8 * max_x);
+
+  for (int x = 0; x < max_x; ++x) {
+    for (int k = 0; k < 8; ++k) {
+      const int xk = 8 * x + k;
+      cr0[xk] = (2.0 * xk) / width - 1.5;
+    }
+  }
+
+
+  for (int y = 0; y < height; ++y) {
+    byte* line = &buffer[y * max_x];
+    spawn([=] {
+      const double ci0 = 2.0 * y / height - 1.0;
+      for (int x = 0; x < max_x; ++x) {
+        const double* cr0_x = &cr0[8 * x];
+        double cr[8];
+        double ci[8];
+        for (int k = 0; k < 8; ++k) {
+            cr[k] = cr0_x[k];
+            ci[k] = ci0;
+        }
+        byte bits = 0xFF;
+        for (int i = 0; bits && i < max_iterations; ++i) {
+          byte bit_k = 0x80;
+          for (int k = 0; k < 8; ++k) {
+            if (bits & bit_k) {
+              const double cr_k    = cr[k];
+              const double ci_k    = ci[k];
+              const double cr_k_sq = cr_k * cr_k;
+              const double ci_k_sq = ci_k * ci_k;
+              cr[k] = cr_k_sq - ci_k_sq + cr0_x[k];
+              ci[k] = 2.0 * cr_k * ci_k + ci0;
+              if (cr_k_sq + ci_k_sq > limit_sq) {
+                bits ^= bit_k;
+              }
+            }
+            bit_k >>= 1;
+          }
+        }
+        line[x] = bits;
+      }
+    });
+  }
+
+  await_all_actors_done();
+  shutdown();
+
+  //FILE* out = (argc == 3) ? fopen(argv[2], "wb") : stdout;
+  //fprintf(out, "P4\n%u %u\n", width, height);
+  //fwrite(&buffer[0], buffer.size(), 1, out);
+  //if (out != stdout) {
+  //  fclose(out);
+  //}
+  return 0;
+}
---
+++ scala/mandelbrot.scala
@@ -0,0 +1,105 @@
+/* The Computer Language Benchmarks Game
+   http://benchmarksgame.alioth.debian.org/
+   original contributed by Isaac Gouy
+   made to use single array and parallelized by Stephen Marsh
+   converted to Scala 2.8 by Rex Kerr
+   added Akka
+*/
+
+package org.caf
+
+import akka.actor._
+import org.caf.utility._
+
+import Array._
+import java.io.BufferedOutputStream
+
+object mandelbrot {
+  
+  var global_latch: java.util.concurrent.CountDownLatch = null
+
+  var size: Int = 0
+  var bytesPerRow: Int = 0
+  var bitmap: Array[Byte] = _
+  var donerows: Array[Boolean] = _
+  var nextRow = 0
+  val limitSquared = 4.0
+  val max = 50
+
+  case class Row(row: Int)
+
+  def usage() = {
+    print("usage: (number of pixels > 0)\n")
+    System.exit(1) // why doesn't exit return Nothing?
+  }
+
+  class Worker extends Actor {
+    def receive = {
+      case Row(row) => {
+        var bits = 0
+        var bitnum = 0
+        var x = 0
+        var aindex = row * bytesPerRow
+        while (x < size) {
+          val cr = 2.0 * x / size - 1.5
+          val ci = 2.0 * row / size - 1.0
+          var zr, tr, zi, ti = 0.0
+          var j = max
+          do {
+            zi = 2.0 * zr * zi + ci
+            zr = tr - ti + cr
+            ti = zi*zi
+            tr = zr*zr
+            j = j - 1
+          } while (!(tr + ti > limitSquared) && j > 0)
+          bits = bits << 1
+          if (!(tr + ti > limitSquared)) {
+            bits += 1
+          }
+          bitnum += 1
+          if (x == size - 1) {
+            bits = bits << (8 - bitnum)
+            bitnum = 8
+          }
+          if (bitnum == 8) {
+            bitmap(aindex) = bits.toByte
+            aindex += 1
+            bits = 0
+            bitnum = 0
+          }
+          x += 1
+        }
+        context stop self
+        global_latch countDown
+      }
+    }
+  }
+
+  def main(args: Array[String]) : Unit = args match {
+    //case Array(IntStr(numPixels)) if numPixels == 0 => usage
+    case Array(IntStr(numPixels)) => {
+      size = numPixels
+      bytesPerRow = (size+7)/8 // ceiling of (size / 8)
+      bitmap = new Array(bytesPerRow*size)
+      donerows = new Array(size)
+      global_latch = new java.util.concurrent.CountDownLatch(numPixels)
+      val system = ActorSystem()
+      for (i <- 0 until size) {
+        system.actorOf(Props[Worker]) ! Row(i)
+      }
+      global_latch.await
+      // main thread prints rows as they finish
+      //println("P4\n" + size + " " + size)
+      //val w = new BufferedOutputStream(System.out)
+      //var y = 0
+      //while (y < size) {
+      // w.write(bitmap, y * bytesPerRow, bytesPerRow)
+      //  y += 1
+      //}
+      //w.close
+      system.shutdown
+      System.exit(0)
+    }
+    case _ => usage
+  }
+}
