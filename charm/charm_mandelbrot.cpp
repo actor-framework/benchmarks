096610c92097fbc5acfb0340421c58fafbedf2dc
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2014-10-06 15:05:19 +0200

    Fix Charm++ mandelbrot benchmark

--- charm/charm_mandelbrot.cpp
+++ charm/charm_mandelbrot.cpp
@@ -6,6 +6,12 @@
 
 typedef unsigned char byte;
 
+namespace {
+size_t m_counter;
+std::vector<byte>   m_buffer;
+std::vector<double> m_cr0;
+}
+
 class main : public CBase_main {
  public:
   main(CkArgMsg* m) {
@@ -21,21 +27,18 @@ class main : public CBase_main {
     int max_iterations = 50;
     double limit = 2.0;
     double limit_sq = limit * limit;
-    std::vector<byte> buffer(N * max_x);
-    std::vector<double> cr0(8 * max_x);
+    m_buffer.resize(N * max_x);
+    m_cr0.resize(8 * max_x);
 
     for (int x = 0; x < max_x; ++x) {
       for (int k = 0; k < 8; ++k) {
         const int xk = 8 * x + k;
-        cr0[xk] = (2.0 * xk) / N - 1.5;
+        m_cr0[xk] = (2.0 * xk) / N - 1.5;
       }
     }
-    std::cout << "Starting with counter creation ..." << std::endl;
-    CProxy_counter cnt = CProxy_counter::ckNew(N, buffer);
-    std::cout << "Starting with worker creation ... " << std::endl;
     for (int y = 0; y < N; ++y) {
-      CkPrintf("%s%i%s", "Creating worker ", y, ".\n");
-      CProxy_worker w = CProxy_worker::ckNew(cnt, N, max_x, max_iterations, limit_sq, buffer, cr0);
+      //CkPrintf("%s%i%s", "Creating worker ", y, ".\n");
+      CProxy_worker w = CProxy_worker::ckNew(N, max_x, max_iterations, limit_sq);
       w.calc(y);
     }
   }
@@ -43,29 +46,21 @@ class main : public CBase_main {
 
 class worker : public CBase_worker {
 
-  CProxy_counter            m_cnt;
   int                        m_dim;
   int                        m_max_x;
   int                        m_max_iters;
   double                     m_limit_sq;
-  //byte*                      m_line;
-  std::vector<byte>&         m_buffer;
-  const std::vector<double>& m_cr0;
 
  public:
-  worker(CProxy_counter cnt, int N, int max_x, int max_iters, 
-         double limit_sq, std::vector<byte>& buffer, const std::vector<double>& cr0)
-    : m_cnt(cnt), 
-      m_dim(N),
+  worker(int N, int max_x, int max_iters, double limit_sq)
+    : m_dim(N),
       m_max_x(max_x),
       m_max_iters(max_iters),
-      m_limit_sq(limit_sq),
-      //m_line(line),
-      m_buffer(buffer),
-      m_cr0(cr0) { }
+      m_limit_sq(limit_sq) {
+    // nop
+  }
 
   void calc(int row) {
-    CkPrintf("%s%i%s", "Starting to calculate row ", row, ".\n");
     byte* line = &m_buffer[row * m_max_x];
     const double ci0 = 2.0 * row / m_dim - 1.0;
     for (int x = 0; x < m_max_x; ++x) {
@@ -96,35 +91,20 @@ class worker : public CBase_worker {
       }
       line[x] = bits;
     }
-    CkPrintf("%s%i%s", "Done with row ", row, ".\n");
-    m_cnt.msg();
     delete this;
-  }
-};
-
-class counter : public CBase_counter {
- public:
-  counter(uint64_t max, const std::vector<byte>& buffer)
-    : m_max(max), m_value(0), m_buffer(buffer) { }
-
-  void msg() {
-    if (++m_value == m_max) {
-      CkPrintf("%s", "Mandelbrot is done\n");
-      FILE* out = stdout;
-      fprintf(out, "P4\n%u %u\n", m_max, m_max);
+    if (__sync_add_and_fetch(&m_counter, 1) == m_dim) {
+      /*
+      FILE* out = fopen("chaming mandelbrot.bmp", "wb");
+      fprintf(out, "P4\n%u %u\n", m_dim, m_dim);
       fwrite(&m_buffer[0], m_buffer.size(), 1, out);
       if (out != stdout) {
         fclose(out);
       }
+      */
+      // done
       CkExit();
     }
-    CkPrintf("%s%i%s", "Received ", m_value, " done messages\n");
   }
-
- private:
-  int m_max;
-  int m_value;
-  const std::vector<byte>& m_buffer;
 };
 
 #include "charm_mandelbrot.def.h"
